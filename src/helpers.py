f75c14c48ef1939b8088a6f25c9edc61dc1d380c
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-12-30 12:52:55 +0100

    Add files via upload

--- src/helpers.py
+++ src/helpers.py
@@ -18,55 +18,55 @@ def parameter_parser():
     parser = argparse.ArgumentParser(description = "Run TADW.")
 
 
-    parser.add_argument('--edge-path',
-                        nargs = '?',
-                        default = './input/chameleon_edges.csv',
-	                help = 'Input edges.')
+    parser.add_argument("--edge-path",
+                        nargs = "?",
+                        default = "./input/chameleon_edges.csv",
+	                help = "Input edges.")
 
-    parser.add_argument('--feature-path',
-                        nargs = '?',
-                        default = './input/chameleon_features.json',
-	                help = 'Input features.')
+    parser.add_argument("--feature-path",
+                        nargs = "?",
+                        default = "./input/chameleon_features.json",
+	                help = "Input features.")
 
-    parser.add_argument('--output-path',
-                        nargs = '?',
-                        default = './output/chameleon_tadw.csv',
-	                help = 'Output embedding.')
+    parser.add_argument("--output-path",
+                        nargs = "?",
+                        default = "./output/chameleon_tadw.csv",
+	                help = "Output embedding.")
 
-    parser.add_argument('--dimensions',
+    parser.add_argument("--dimensions",
                         type = int,
                         default = 32,
-	                help = 'Number of dimensions. Default is 32.')
+	                help = "Number of dimensions. Default is 32.")
 
-    parser.add_argument('--order',
+    parser.add_argument("--order",
                         type = int,
                         default = 2,
-	                help = 'Target matrix approximation order. Default is 2.')
+	                help = "Target matrix approximation order. Default is 2.")
 
-    parser.add_argument('--iterations',
+    parser.add_argument("--iterations",
                         type = int,
                         default = 20,
-	                help = 'Number of gradient descent iterations. Default is 20.')
+	                help = "Number of gradient descent iterations. Default is 20.")
 
-    parser.add_argument('--lambd',
+    parser.add_argument("--lambd",
                         type = float,
                         default = 1000.0,
-	                help = 'Regularization term coefficient. Default is 1000.')
+	                help = "Regularization term coefficient. Default is 1000.")
 
-    parser.add_argument('--alpha',
+    parser.add_argument("--alpha",
                         type = float,
                         default = 10**-6,
-	                help = 'Learning rate. Default is 10^-6.')
+	                help = "Learning rate. Default is 10^-6.")
 
-    parser.add_argument('--features',
-                        nargs = '?',
-                        default = 'sparse',
-	                help = 'Output embedding.')
+    parser.add_argument("--features",
+                        nargs = "?",
+                        default = "sparse",
+	                help = "Output embedding.")
 
-    parser.add_argument('--lower-control',
+    parser.add_argument("--lower-control",
                         type = float,
                         default = 10**-15,
-	                help = 'Overflow control. Default is 10**-15.')
+	                help = "Overflow control. Default is 10**-15.")
     
     return parser.parse_args()
 
